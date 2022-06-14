8393b7a90e4e2b418b7c9ac1bf9d3475f9f996b4
hamiat <70755521+hamiat@users.noreply.github.com>
2022-06-14 20:54:00 +0200

    wip: treat-temp completed

--- src/components/Treats/Treat.jsx
+++ src/components/Treats/Treat.jsx
@@ -1,9 +1,23 @@
 import style from "./treats.module.scss";
 
-export default function Treat({ title }) {
+export default function Treat({ title, picOne, picTwo, picThree }) {
   return (
     <div className={style.treat}>
       <h2>{title}</h2>
+      <ul className={style.treatList}>
+        <li>
+          <img src={picOne} alt="Baked goods" />
+          <p>Homey carrot cake with cinnamon. </p>
+        </li>
+        <li>
+          <img src={picTwo} alt="Baked goods" />
+          <p>Homey carrot cake with cinnamon. </p>
+        </li>
+        <li>
+          <img src={picThree} alt="Baked goods" />
+          <p>Homey carrot cake with cinnamon. </p>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/components/Treats/Treats.jsx
+++ src/components/Treats/Treats.jsx
@@ -4,9 +4,21 @@ import style from "./treats.module.scss";
 
 export default function Treats() {
   const [treatTemps, setTreatTemp] = useState([
-    { id: 1, title: "Pastries", bgColor: "#FAF3F3" },
-    { id: 2, title: "Cupcakes", bgColor: "#F6F8F9" },
-    { id: 3, title: "Donuts", bgColor: "#F7F7F2" }
+    {
+      id: 1,
+      title: "Pastries",
+      bgColor: "#FAF3F3",
+      picOne: "/assets/pink-4.svg",
+      picTwo: "/assets/pink-2.svg",
+      picThree: "/assets/pink-3.svg"
+    },
+    {
+      id: 2,
+      title: "Cupcakes",
+      bgColor: "#F6F8F9",
+      picOne: "/assets/pink-1.svg"
+    },
+    { id: 3, title: "Donuts", bgColor: "#F7F7F2", picOne: "/assets/pink-1.svg" }
   ]);
 
   return (
