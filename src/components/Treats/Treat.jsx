23c414526db281808e549c735555f3f767c77517
hamiat <hamiat.n@hotmail.com>
2022-06-22 16:54:21 +0200

    wip: temp prices and title added

--- src/components/Treats/Treat.jsx
+++ src/components/Treats/Treat.jsx
@@ -9,17 +9,21 @@ export default function Treat({ title, picOne, picTwo, picThree }) {
       <h2 className={style.sectionTitle}>{title}</h2>
       <ul className={style.treatList}>
         <li>
+        
           <img src={picOne.url} alt="Baked goods" />
+          <h4>Yummy Yum <span>4 &#164;</span></h4>
           <p className={style.treatText}>{picOne.description} </p>
           <CartBtns />
         </li>
         <li>
           <img src={picTwo.url} alt="Baked goods" />
+          <h4>Yummy Yum <span>8 &#164;</span></h4>
           <p className={style.treatText}>{picTwo.description} </p>
           <CartBtns />
         </li>
         <li>
           <img src={picThree.url} alt="Baked goods" />
+          <h4>Yummy Yum <span>12 &#164;</span></h4>
           <p className={style.treatText}>{picThree.description} </p>
           <CartBtns />
         </li>
